123c45e615b7104bd06135b4c7329df6e5da9ad1
Jonny Noone <jonny.f.noone@gmail.com>
2021-06-07 20:27:39 +0100

    ickle bickle first commickle

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+
+// This is a constant
+DEFINE("TEST", "We are getting things moving. This is juice.");
+
+DEFINE("DB_HOST", "localhost");
+DEFINE("DB_USER", "root");
+DEFINE("DB_PASS", "");
+DEFINE("DB_NAME", "start");
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include "database.php";
+
+echo "Connected...";
+echo "<br>";
+
+$name = "Jonny";
+
+echo "Welcome, $name!";
+echo "<br>" . TEST;
