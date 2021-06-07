4010f5d385ae4341cbec18a6165bf21d84843323
Jonny Noone <jonny.f.noone@gmail.com>
2021-06-07 21:57:15 +0100

    got that DB rolling, simple READ

--- database.php
+++ database.php
@@ -7,3 +7,10 @@ DEFINE("DB_HOST", "localhost");
 DEFINE("DB_USER", "root");
 DEFINE("DB_PASS", "");
 DEFINE("DB_NAME", "start");
+
+// Connect to DB
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if(mysqli_connect_errno()) {
+    die("Failed to connect: " . mysqli_connect_error());
+}
--- index.php
+++ index.php
@@ -7,5 +7,30 @@ echo "<br>";
 
 $name = "Jonny";
 
-echo "Welcome, $name!";
-echo "<br>" . TEST;
+$query = "SELECT * FROM walking";
+$result = mysqli_query($connect, $query);
+
+echo "
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Walked</th>
+            </tr>
+        </thead>
+
+        <tbody>
+    ";
+
+while($row = mysqli_fetch_assoc($result)) {
+    echo "
+        <tr>
+            <td>" . $row['id'] . "</td>
+            <td>" . $row['date'] . "</td>
+            <td>" . $row['walked'] . "</td>
+        </tr>
+    ";
+}
+
+echo "</tbody></table>";
