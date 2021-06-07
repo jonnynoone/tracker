d18a299ab69b58be57a51dfb7b3db78757c5a614
Jonny Noone <jonny.f.noone@gmail.com>
2021-06-07 22:23:24 +0100

    cleaned up formatting

--- index.php
+++ index.php
@@ -2,14 +2,38 @@
 
 include "database.php";
 
-$query = "SELECT * FROM walking";
+$query = "SELECT * FROM walking ORDER BY date ASC";
 $result = mysqli_query($connect, $query);
 
-echo "
-    <table>
+// Convert BOOLEAN to SYMBOL
+function convBool($boolean) {
+    if($boolean == 1) {
+        $symbol = 'X';
+    } else {
+        $symbol = 'O';
+    }
+
+    return $symbol;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>Tracker</title>
+</head>
+
+<body>
+    <table id="tracker">
         <thead>
             <tr>
-                <th>ID</th>
                 <th>Date</th>
                 <th>Walk</th>
                 <th>Reverse K</th>
@@ -17,17 +41,19 @@ echo "
         </thead>
 
         <tbody>
-    ";
+            <?php
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "
+                    <tr>
+                        <td>" . $row['date'] . "</td>
+                        <td>" . convBool($row['walk']) . "</td>
+                        <td>" . convBool($row['revk']) . "</td>
+                    </tr>
+                ";
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
 
-while($row = mysqli_fetch_assoc($result)) {
-    echo "
-        <tr>
-            <td>" . $row['id'] . "</td>
-            <td>" . $row['date'] . "</td>
-            <td>" . $row['walk'] . "</td>
-            <td>" . $row['revk'] . "</td>
-        </tr>
-    ";
-}
-
-echo "</tbody></table>";
+</html>
